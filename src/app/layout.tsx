2f7f7d138b45a85df5f65aeb9e70659464df6730
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-04-19 23:04:23 +0530

    feat: refactor project structure for better readability and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
+
+const urbanist = Urbanist({
+  display: "swap",
+  weight: "variable",
+  subsets: ["latin"],
+  variable: "--font-urbanist",
+});
 
 export const metadata: Metadata = {
   title: "Minimal Single Page Portfolio",
@@ -13,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={"antialiased"}>{children}</body>
+      <body className={`antialiased bg-stone-200 text-stone-900 ${urbanist.variable} font-sans`}>{children}</body>
     </html>
   );
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 
-/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const navItems = [
   {
     label: "About",
@@ -25,7 +24,30 @@ const navItems = [
 ];
 
 const Header: FC = () => {
-  return <div>Header</div>;
+  return <header>
+    <div className="container !max-w-full">
+      <div className="flex justify-between h-20 items-center">
+        <div>
+          <a href="/">
+            <span className="text-2xl font-bold ">
+              01shrvan
+            </span>
+          </a>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="size-11 border border-stone-400 rounded-full items-center justify-center inline-flex">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect x="3" y="15" width="18" height="2" fill="currentColor" />
+              <rect x="3" y="7" width="18" height="2" fill="currentColor" />
+            </svg>
+          </div>
+          <button className="bg-red-orange-500 h-11 px-6 rounded-xl text-white border border-red-orange-500 hidden md:inline-flex items-center">
+            hmu
+          </button>
+        </div>
+      </div>
+    </div>
+  </header >;
 };
 
 export default Header;
