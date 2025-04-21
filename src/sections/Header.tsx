199dbdd4a69799aa1ebef6b16ffe6b0d6d25249c
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-04-21 22:13:48 +0530

    fix: update Header and Hero sections for improved styling and layout

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -37,7 +37,7 @@ const Header: FC = () => {
             </a>
           </div>
           <div className="flex items-center gap-4">
-            <div className="size-11 border border-stone-400 rounded-full items-center justify-center inline-flex">
+            <div className="size-11 border border-stone-400 rounded-full items-center justify-center inline-flex bg-stone-200">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="3" y="15" width="18" height="2" fill="currentColor" />
                 <rect x="3" y="7" width="18" height="2" fill="currentColor" />
