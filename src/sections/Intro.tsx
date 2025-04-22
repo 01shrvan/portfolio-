11e5335c38e8dd7c50e906e065c8ecf3b273885b
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-04-22 22:20:21 +0530

    feat: add Intro component and integrate it into Home page && update Hero section for consistent styling

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,7 +1,33 @@
 import { FC } from "react";
 
 const Intro: FC = () => {
-  return <div>Intro</div>;
+  return (
+    <section className="py-24 md:py-32 mt-12 lg:py-40 lg:mt-20 md:mt-16">
+      <div className="container">
+        <h2 className="text-4xl md:text-7xl lg:text-8xl lg:w-[80%]">
+          solo dev buildin responsive{" "}
+          <a
+            href="https://nextjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="decoration-wavy underline underline-offset-4 decoration-gray-400"
+          >
+            nextjs
+          </a>{" "}
+          x{" "}
+          <a
+            href="https://tailwindcss.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="decoration-wavy underline underline-offset-4 decoration-gray-400"
+          >
+            tailwind
+          </a>{" "}
+          landing pages tailored to ur brand vibe and goals
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
