6203940dc15437b7bf28b6f101caf2a3e1bd4a3a
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-05-13 21:52:57 +0530

    style: update Header component for improved visual consistency

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -26,16 +26,23 @@ const navItems = [
 
 const Header: FC = () => {
   return (
-    <header className="fixed top-0 left-0 w-full backdrop-blur-md">
-      <div className="container !max-w-full">
-        <div className="flex justify-between h-20 items-center">
-          <div>
-            <a href="/">
-              <span className="text-2xl font-bold ">
-                01shrvan
-              </span>
-            </a>
+    <header>
+      <div className="fixed top-0 left-0 w-full mix-blend-difference backdrop-blur-md">
+        <div className="container !max-w-full">
+          <div className="flex justify-between h-20 items-center">
+            <div>
+              <a href="/">
+                <span className="text-2xl font-bold text-white">
+                  01shrvan
+                </span>
+              </a>
+            </div>
           </div>
+        </div>
+      </div>
+      <div className="fixed top-0 left-0 w-full">
+        <div className="container !max-w-full">
+        <div className="flex justify-end h-20 items-center">
           <div className="flex items-center gap-4">
             <div className="size-11 border border-stone-400 rounded-full items-center justify-center inline-flex bg-stone-200">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -49,6 +56,8 @@ const Header: FC = () => {
           </div>
         </div>
       </div>
+      </div>
+      
     </header >
   );
 };
